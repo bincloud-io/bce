71a0bc5f3d4a16e1bfc1755e9d749483910452ff
Dmitry Mikhaylenko <dmikhaylenko@hotmail.com>
2022-12-12 16:25:15 +0400

    Add commons library

---
+++ bangover-commons/src/main/java/cloud/bangover/commons/App.java
@@ -0,0 +1,13 @@
+package cloud.bangover.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
